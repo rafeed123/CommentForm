a30d928279a7fed94a674bc2cabfe17d509c5c32
Rafee Daanish <rafee.daanish@gmail.com>
2017-02-07 14:29:54 -0500

    added properties to my CommitFormModel Class

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 
 namespace CommentForm.Models
 {
     public class CommentFormModel
     {
+        //Our ID keeps track of each instance
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
+
     }
 }
